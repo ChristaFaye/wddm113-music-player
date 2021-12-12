6d9fede0ceba749910ed76291c4ff9bb706b1d38
christafayereyes <christafayereyes@gmail.com>
2021-12-12 12:16:13 -0600

    Copied array to clicked function to retrieve data

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+
+
+
 window.addEventListener('DOMContentLoaded', function() {
 
 const songList = [
@@ -12,7 +15,133 @@ const songList = [
             duration: "3:00"
         },
         { //song2
+            title: "title2", 
+            artist: "artist2",
+            album: "album2",
+            src: "mp3", 
+            songCover: "img1",
+            desc: "lorem ipsum",
+            rating: "5.0",
+            duration: "3:00"
+        },
+        { //song3
             title: "title1", 
+            artist: "artist1",
+            album: "album1",
+            src: "mp3", 
+            songCover: "img1",
+            desc: "lorem ipsum",
+            rating: "5.0",
+            duration: "3:00"
+        },
+        { //song4
+            title: "title1", 
+            artist: "artist1",
+            album: "album1",
+            src: "mp3", 
+            songCover: "img1",
+            desc: "lorem ipsum",
+            rating: "5.0",
+            duration: "3:00"
+        },
+        { //song5
+            title: "title1", 
+            artist: "artist1",
+            album: "album1",
+            src: "mp3", 
+            songCover: "img1",
+            desc: "lorem ipsum",
+            rating: "5.0",
+            duration: "3:00"
+        },
+        { //song6
+            title: "title1", 
+            artist: "artist1",
+            album: "album1",
+            src: "mp3", 
+            songCover: "img1",
+            desc: "lorem ipsum",
+            rating: "5.0",
+            duration: "3:00"
+        },
+        { //song7
+            title: "title1", 
+            artist: "artist1",
+            album: "album1",
+            src: "mp3", 
+            songCover: "img1",
+            desc: "lorem ipsum",
+            rating: "5.0",
+            duration: "3:00"
+        },
+        { //song8
+            title: "title1", 
+            artist: "artist1",
+            album: "album1",
+            src: "mp3", 
+            songCover: "img1",
+            desc: "lorem ipsum",
+            rating: "5.0",
+            duration: "3:00"
+        },
+        { //song9
+            title: "title1", 
+            artist: "artist1",
+            album: "album1",
+            src: "mp3", 
+            songCover: "img1",
+            desc: "lorem ipsum",
+            rating: "5.0",
+            duration: "3:00"
+        },
+        { //song10
+            title: "title1", 
+            artist: "artist1",
+            album: "album1",
+            src: "mp3", 
+            songCover: "img1",
+            desc: "lorem ipsum",
+            rating: "5.0",
+            duration: "3:00"
+        }    
+];
+
+let list = document.getElementById("list");
+var i =0;
+songList.forEach((item)=>{
+    i++;
+    let li = document.createElement("li");
+    li.setAttribute(`id`, `song${i}`);
+    li.setAttribute(`onclick`, `clicked(this.id)`);
+    li.innerHTML = `
+        <img src=${item.songCover} id="imglist" "/>
+        <span>
+            <h2>${item.title}</h2>
+            <h3>${item.album} by ${item.artist}</h3>
+        </span>
+        <p>${item.duration}</p>
+        
+    `;
+    list.appendChild(li);
+  })
+
+
+});
+
+function clicked(clicked_id) { 
+const songList = [
+        {  //song1
+            title: "title1", 
+            artist: "artist1",
+            album: "album1",
+            src: "mp3", 
+            songCover: "../img/song-cover.jpg",
+            desc: "lorem ipsum",
+            rating: "5.0",
+            duration: "3:00"
+        },
+        { //song2
+            title: "title2", 
             artist: "artist1",
             album: "album1",
             src: "mp3", 
@@ -101,38 +230,23 @@ const songList = [
             rating: "5.0",
             duration: "3:00"
         }    
-    ];
-
-    
-let list = document.getElementById("list");
-var i =0;
-songList.forEach((item)=>{
-    i++;
-    let li = document.createElement("li");
-    li.setAttribute(`id`, `song${i}`);
-    li.setAttribute(`onclick`, `selectedSong(this.id)`);
-    li.innerHTML = `
-        <img src=${item.songCover} id="imglist" "/>
-        <span>
-            <h2>${item.title}</h2>
-            <h3>${item.album} by ${item.artist}</h3>
-        </span>
-        <p>${item.duration}</p>
-        
-    `;
-    list.appendChild(li);
-    
-  })
-  return num;
-});
-
-function selectedSong(clicked_id) { 
+];
     var num = clicked_id.charAt(4) + clicked_id.charAt(5);
-    document.getElementById("songTitle").innerHTML = num-1;
-    return num;
+    var x = num - 1;
+    console.log(songList[x]);
+    var song = songList[x];
+    document.getElementById("songTitle").innerHTML = song.title;
+    document.getElementById("imgPlaying").src = song.src;
+    document.getElementById("artist").innerHTML = song.artist;
+    document.getElementById("album").innerHTML = song.album;
+    
 }
 
-console.log(num);
+
+
+
+
+
 
 
 
