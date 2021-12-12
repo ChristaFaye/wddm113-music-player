10d45e3115c03a434c18caca8ef30e51e59ec0cc
christafayereyes <christafayereyes@gmail.com>
2021-12-11 18:57:26 -0600

    Added contentload listener to load js upon window load, added foreach loop to create li element for each song details

--- js/script.js
+++ js/script.js
@@ -1,92 +1,117 @@
-const songList = [
-    {  //song1
-        title: "title1", 
-        artist: "artist1",
-        album: "album1",
-        src: "mp3", 
-        songCover: "img1",
-        desc: "lorem ipsum",
-        rating: "5.0",
-    },
-    { //song2
-        title: "title1", 
-        artist: "artist1",
-        album: "album1",
-        src: "mp3", 
-        songCover: "img1",
-        desc: "lorem ipsum",
-        rating: "5.0",
-    },
-    { //song3
-        title: "title1", 
-        artist: "artist1",
-        album: "album1",
-        src: "mp3", 
-        songCover: "img1",
-        desc: "lorem ipsum",
-        rating: "5.0",
-    },
-    { //song4
-        title: "title1", 
-        artist: "artist1",
-        album: "album1",
-        src: "mp3", 
-        songCover: "img1",
-        desc: "lorem ipsum",
-        rating: "5.0",
-    },
-    { //song5
-        title: "title1", 
-        artist: "artist1",
-        album: "album1",
-        src: "mp3", 
-        songCover: "img1",
-        desc: "lorem ipsum",
-        rating: "5.0",
-    },
-    { //song6
-        title: "title1", 
-        artist: "artist1",
-        album: "album1",
-        src: "mp3", 
-        songCover: "img1",
-        desc: "lorem ipsum",
-        rating: "5.0",
-    },
-    { //song7
-        title: "title1", 
-        artist: "artist1",
-        album: "album1",
-        src: "mp3", 
-        songCover: "img1",
-        desc: "lorem ipsum",
-        rating: "5.0",
-    },
-    { //song8
-        title: "title1", 
-        artist: "artist1",
-        album: "album1",
-        src: "mp3", 
-        songCover: "img1",
-        desc: "lorem ipsum",
-        rating: "5.0",
-    },
-    { //song9
-        title: "title1", 
-        artist: "artist1",
-        album: "album1",
-        src: "mp3", 
-        songCover: "img1",
-        desc: "lorem ipsum",
-        rating: "5.0",
-    },
-    { //song10
-        title: "title1", 
-        artist: "artist1",
-        album: "album1",
-        src: "mp3", 
-        songCover: "img1",
-        desc: "lorem ipsum",
-        rating: "5.0",
-    }    
-]
+window.addEventListener('DOMContentLoaded', function() {
+
+let songList = [
+        {  //song1
+            title: "title1", 
+            artist: "artist1",
+            album: "album1",
+            src: "mp3", 
+            songCover: "img1",
+            desc: "lorem ipsum",
+            rating: "5.0",
+        },
+        { //song2
+            title: "title1", 
+            artist: "artist1",
+            album: "album1",
+            src: "mp3", 
+            songCover: "img1",
+            desc: "lorem ipsum",
+            rating: "5.0",
+        },
+        { //song3
+            title: "title1", 
+            artist: "artist1",
+            album: "album1",
+            src: "mp3", 
+            songCover: "img1",
+            desc: "lorem ipsum",
+            rating: "5.0",
+        },
+        { //song4
+            title: "title1", 
+            artist: "artist1",
+            album: "album1",
+            src: "mp3", 
+            songCover: "img1",
+            desc: "lorem ipsum",
+            rating: "5.0",
+        },
+        { //song5
+            title: "title1", 
+            artist: "artist1",
+            album: "album1",
+            src: "mp3", 
+            songCover: "img1",
+            desc: "lorem ipsum",
+            rating: "5.0",
+        },
+        { //song6
+            title: "title1", 
+            artist: "artist1",
+            album: "album1",
+            src: "mp3", 
+            songCover: "img1",
+            desc: "lorem ipsum",
+            rating: "5.0",
+        },
+        { //song7
+            title: "title1", 
+            artist: "artist1",
+            album: "album1",
+            src: "mp3", 
+            songCover: "img1",
+            desc: "lorem ipsum",
+            rating: "5.0",
+        },
+        { //song8
+            title: "title1", 
+            artist: "artist1",
+            album: "album1",
+            src: "mp3", 
+            songCover: "img1",
+            desc: "lorem ipsum",
+            rating: "5.0",
+        },
+        { //song9
+            title: "title1", 
+            artist: "artist1",
+            album: "album1",
+            src: "mp3", 
+            songCover: "img1",
+            desc: "lorem ipsum",
+            rating: "5.0",
+        },
+        { //song10
+            title: "title1", 
+            artist: "artist1",
+            album: "album1",
+            src: "mp3", 
+            songCover: "img1",
+            desc: "lorem ipsum",
+            rating: "5.0",
+        }    
+    ];
+
+    
+let list = document.getElementById("list");
+
+songList.forEach((item)=>{
+    let li = document.createElement("li");
+    li.innerHTML = `
+    <img src="${item.src}"/>
+    <h2>${item.title}</h2>
+    <h3>${item.album} by ${item.artist}</h3>
+    <p>${item.rating}`;
+    //li.setAttribute(src, item.src);
+    
+    
+    list.appendChild(li);
+  })
+
+// for(let i = 0; i < songList.length; i++){
+//     console.log(songList[i].title);
+//     document.getElementById("song").innerHTML = "TEST2";
+// } 
+
+});
